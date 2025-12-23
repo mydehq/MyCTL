7c4c490102b809706748967d21171337bf66aa57
Soumadip Das <soumadip@zohomail.in>
2025-12-23 16:44:22 +0530

    fix: fix bump script

--- .github/scripts/bump-pkgbuild.sh
+++ .github/scripts/bump-pkgbuild.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-NEW_VERSION="${1:$CZ_PRE_NEW_VERSION}"
+NEW_VERSION="${1:-$CZ_PRE_NEW_VERSION}"
 
 # Check if version was passed
 if [ -z "$NEW_VERSION" ]; then
@@ -10,7 +10,7 @@ if [ -z "$NEW_VERSION" ]; then
     exit 1
 fi
 
-echo -e "\033[38;5;244m => Bumping PKGBUILD to: ${NEW_VERSION}...\033[0m"
+echo -e "\033[38;5;244m -> Bumping PKGBUILD to ${NEW_VERSION}...\033[0m"
 
 # 1. Update pkgver
 sed -i "s/^ *pkgver=.*/pkgver=${NEW_VERSION}/" PKGBUILD
@@ -21,4 +21,4 @@ sed -i "s/^ *pkgrel=.*/pkgrel=1/" PKGBUILD
 # 4. Stage the file for the release commit
 git add PKGBUILD
 
-echo -e "\033[0;32m => PKGBUILD updated and staged.\033[0m"
+echo -e "\033[0;32m -> PKGBUILD updated and staged.\033[0m"
